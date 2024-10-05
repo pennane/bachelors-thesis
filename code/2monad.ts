63db469ec67f3c3aba183b85428be87c22480b7b
Arttu Pennanen <arttu@pennanen.org>
2024-10-05 18:29:58 +0300

    progress research part considerably

--- code/2monad.ts
+++ code/2monad.ts
@@ -9,7 +9,7 @@ type Success<T> = { type: 'success'; value: T }
 type Failure = { type: 'failure'; error: Error }
 type Result<T> = Success<T> | Failure
 
-const chain =
+const bind =
   <A, B>(f: (x: A) => Result<B>) =>
   (x: Result<A>): Result<B> => {
     if (x.type === 'failure') return x
@@ -21,16 +21,16 @@ const unwrap = <T>(x: Result<T>): T => {
   return x.value
 }
 
-const success = <T>(value: T): Success<T> => ({ type: 'success', value })
-const failure = (error: string): Failure => ({
+const successOf = <T>(value: T): Success<T> => ({ type: 'success', value })
+const failureOf = (error: string): Failure => ({
   type: 'failure',
   error: new Error(error)
 })
 
 const deserializeHeader = (header: string): Result<AuthHeader> => {
   const delimiterIndex = header.indexOf(' ')
-  if (delimiterIndex === -1) return failure('Auth header is malformed.')
-  return success({
+  if (delimiterIndex === -1) return failureOf('Auth header is malformed.')
+  return successOf({
     scheme: header.slice(0, delimiterIndex),
     value: header.slice(delimiterIndex)
   })
@@ -50,22 +50,22 @@ const validateScheme =
   (opts: SchemaValidationOptions) =>
   (header: AuthHeader): Result<AuthHeader> => {
     if (!schemeMatches(opts)(header.scheme)) {
-      return failure('Unexpected auth scheme')
+      return failureOf('Unexpected auth scheme')
     }
-    return success(header)
+    return successOf(header)
   }
 
-const getValue = (header: AuthHeader): Result<string> => success(header.value)
+const getValue = (header: AuthHeader): Result<string> => successOf(header.value)
 
 const parseAuthTokenFp = pipe([
   deserializeHeader,
-  chain(
+  bind(
     validateScheme({
       allowedSchemes: ['Bearer', 'Access'],
       caseInsensitive: true
     })
   ),
-  chain(getValue),
+  bind(getValue),
   unwrap
 ])
 
